e67a01d6e6cc75e612ec6bc1b645741490e36164
gesheng <gesheng199832@126.com>
2026-09-02 15:06:03 +0800

    init

--- build.zig
+++ build.zig
@@ -14,55 +14,55 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&run_tests.step);
 
-    const qp = b.addExecutable(.{
-        .name = "qp",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("examples/qp.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{.{ .name = "zad", .module = mod }},
-        }),
-    });
-    const qp_run = b.addRunArtifact(qp);
-    const qp_step = b.step("qp", "Run the quadratic program example");
-    qp_step.dependOn(&qp_run.step);
+    // const qp = b.addExecutable(.{
+    //     .name = "qp",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("examples/qp.zig"),
+    //         .target = target,
+    //         .optimize = optimize,
+    //         .imports = &.{.{ .name = "zad", .module = mod }},
+    //     }),
+    // });
+    // const qp_run = b.addRunArtifact(qp);
+    // const qp_step = b.step("qp", "Run the quadratic program example");
+    // qp_step.dependOn(&qp_run.step);
 
-    const partial_grad = b.addExecutable(.{
-        .name = "partial-grad",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("examples/partial_grad.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{.{ .name = "zad", .module = mod }},
-        }),
-    });
-    const partial_grad_run = b.addRunArtifact(partial_grad);
-    const partial_grad_step = b.step("partial-grad", "Run the partial gradient example");
-    partial_grad_step.dependOn(&partial_grad_run.step);
+    // const partial_grad = b.addExecutable(.{
+    //     .name = "partial-grad",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("examples/partial_grad.zig"),
+    //         .target = target,
+    //         .optimize = optimize,
+    //         .imports = &.{.{ .name = "zad", .module = mod }},
+    //     }),
+    // });
+    // const partial_grad_run = b.addRunArtifact(partial_grad);
+    // const partial_grad_step = b.step("partial-grad", "Run the partial gradient example");
+    // partial_grad_step.dependOn(&partial_grad_run.step);
 
-    const benchmark = b.addExecutable(.{
-        .name = "eval-bench",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("benches/eval.zig"),
-            .target = target,
-            .optimize = .ReleaseFast,
-            .imports = &.{.{ .name = "zad", .module = mod }},
-        }),
-    });
-    const benchmark_run = b.addRunArtifact(benchmark);
-    const benchmark_step = b.step("bench", "Run the VM evaluation benchmark");
-    benchmark_step.dependOn(&benchmark_run.step);
+    // const benchmark = b.addExecutable(.{
+    //     .name = "eval-bench",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("benches/eval.zig"),
+    //         .target = target,
+    //         .optimize = .ReleaseFast,
+    //         .imports = &.{.{ .name = "zad", .module = mod }},
+    //     }),
+    // });
+    // const benchmark_run = b.addRunArtifact(benchmark);
+    // const benchmark_step = b.step("bench", "Run the VM evaluation benchmark");
+    // benchmark_step.dependOn(&benchmark_run.step);
 
-    const asm_object = b.addObject(.{
-        .name = "eval-bench-asm",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("benches/asm.zig"),
-            .target = target,
-            .optimize = .ReleaseFast,
-            .imports = &.{.{ .name = "zad", .module = mod }},
-        }),
-    });
-    const install_asm = b.addInstallFile(asm_object.getEmittedAsm(), "eval-bench.s");
-    const asm_step = b.step("bench-asm", "Emit VM evaluation assembly");
-    asm_step.dependOn(&install_asm.step);
+    // const asm_object = b.addObject(.{
+    //     .name = "eval-bench-asm",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("benches/asm.zig"),
+    //         .target = target,
+    //         .optimize = .ReleaseFast,
+    //         .imports = &.{.{ .name = "zad", .module = mod }},
+    //     }),
+    // });
+    // const install_asm = b.addInstallFile(asm_object.getEmittedAsm(), "eval-bench.s");
+    // const asm_step = b.step("bench-asm", "Emit VM evaluation assembly");
+    // asm_step.dependOn(&install_asm.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,36 +1,3 @@
-pub const dag = @import("dag.zig");
-pub const ir = @import("ir.zig");
-pub const ir_opt = @import("ir_opt.zig");
-pub const ir_grad = @import("ir_grad.zig");
-pub const vm = @import("vm.zig");
-pub const compiler = @import("compiler.zig");
+const ir = @import("ir");
 
-pub const Scalar = dag.Scalar;
-pub const Vector = dag.Vector;
-pub const Vec = dag.Vector;
-pub const Matrix = dag.Matrix;
-pub const Mat = dag.Matrix;
-pub const to_dag = dag.toDag;
-
-pub const CompileOptions = compiler.CompileOptions;
-pub const GradOptions = compiler.GradOptions;
-pub const GradMode = compiler.GradMode;
-pub const GradSelection = compiler.GradSelection;
-pub const compile = compiler.compile;
-pub const grad = compiler.grad;
-pub const eval = vm.eval;
-pub const eval_into = vm.evalInto;
-pub const eval_with_workspace = vm.evalWithWorkspace;
-pub const eval_flat = vm.evalFlat;
-pub const eval_flat_into = vm.evalFlatInto;
-pub const eval_flat_with_workspace = vm.evalFlatWithWorkspace;
-pub const Workspace = vm.Workspace;
-
-test {
-    _ = dag;
-    _ = ir;
-    _ = ir_opt;
-    _ = ir_grad;
-    _ = vm;
-    _ = compiler;
-}
+test "all" {}
